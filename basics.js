815c19092d100bb147a4d8583ebbb2c0f6b5886f
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2024-05-02 15:54:04 -0400

    Add JavaScript Basics

---
+++ basics.js
@@ -0,0 +1,120 @@
+// let num = 10.5;
+
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+// console.log(Math.round(num));
+// console.log(Math.random());
+
+// let strNum = '10.8px';
+// console.log(Number(strNum));
+// console.log(parseInt(strNum));
+// console.log(parseFloat(strNum));
+
+// console.log(BigInt(100));
+
+// console.log(typeof 100);
+
+let str = 'abcd';
+
+// console.log(str + 'efg');
+// console.log(10 + 10 + str);
+// console.log(str + 10 + 10);
+
+// console.log(str < 'abcde');
+// console.log(str === 'abcd');
+// console.log("Hello this is a single quote: '");
+// console.log('Hello \nworld');
+// console.log(`Hello
+// world`);
+// console.log('a' > 'A');
+// console.log('This is a very ' + 'long string');
+
+// console.log(str[0]);
+// console.log(str.charAt(0));
+// console.log(str.includes('a'));
+// console.log(str.startsWith('a'));
+// console.log(str.toUpperCase());
+// console.log(str.substring(1, 2));
+// console.log(str.slice(1, 2));
+// console.log(str.padStart(10, '*'));
+// console.log(str.padEnd(10, '*'));
+// console.log(str.trim());
+// console.log(str.split(''));
+
+// const person = {
+//   name: 'Andrey',
+//   age: 38,
+// };
+
+// console.log(person);
+// console.log(JSON.stringify(person));
+
+// const map = new Map();
+// map.set('name', 'Andrey');
+// console.log(map);
+// console.log(map.get('name'));
+// console.log(map.size);
+
+// const set = new Set();
+// set.add('Andrey');
+// set.add('Andrey');
+// set.add('Andrey');
+
+// console.log(set);
+
+// const setStr = new Set(str);
+// console.log(setStr);
+// console.log(setStr.size);
+
+// const arr = [1, 2, 3];
+// arr.push(4);
+// console.log(arr);
+// console.log(arr.lenght);
+// console.log(arr[1]);
+
+// function addTwo(num = 6) {
+//   return num + 2;
+// }
+
+// function callFunc(func, param) {
+//   console.log(func(param));
+// }
+
+// callFunc(addTwo, 2);
+
+// for (let i = 0; i < 4; i++) {
+//   console.log(i);
+//   if (i == 1) {
+//     break;
+//   }
+//   if (i == 1) {
+//     continue;
+//   }
+// }
+
+// let i = 0;
+// while (i < 4) {
+//   console.log(i);
+//   i++;
+// }
+
+// for (const char of ['a', 'b', 'c']) {
+//   console.log(char);
+// }
+
+// [1, 2, 3].forEach((value) => console.log(value));
+
+// try {
+//   throw new Error('Oh no');
+// } catch (error) {
+//   console.log(error);
+// }
+
+// console.time();
+// console.timeLog();
+
+// function foo() {
+//   console.trace();
+// }
+
+// foo();
