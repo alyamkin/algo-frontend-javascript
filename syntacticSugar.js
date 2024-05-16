58c0e30f249019e01f1b642fa68b11e4b04e2b4d
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2024-05-15 21:30:24 -0400

    Add Syntactic Sugar And Modern JavaScript

---
+++ syntacticSugar.js
@@ -0,0 +1,58 @@
+import { log } from './log.js';
+
+// const arr = [1, 2, 3, 4];
+// const doubled = arr.map((num) => num * 2);
+// log(doubled);
+
+// const person = {
+//   name: 'Conner',
+//   website: 'FrontendExport',
+// };
+// const [first, second, ...rest] = arr;
+// log(first, second, rest);
+// log(arr);
+// const { name: firstName, company = 'AlgoExpert', ...rest } = person;
+// log(firstName, company, rest);
+
+// function printName({ name }) {
+//   log(name);
+// }
+// printName(person);
+
+// const arr = [1, 2, 3, 4];
+// function add(x, y) {
+//   log(x + y);
+// }
+// add(...arr);
+
+// const arr = [1, 2, 3, 4];
+// const arr2 = [5, 6, 7];
+// const combined = [0, ...arr, ...arr2];
+// log(combined);
+
+// const arr = [1, 2, 3, 4];
+// function logParam(x, ...rest) {
+//   log(x);
+//   log(rest);
+// }
+// logParam(1, 2, 3, 4, 5);
+
+// const name = 'Conner';
+// const defaultName = name ?? 'Default Name';
+// log(`Hello ${defaultName}`);
+
+// const person = {
+//   company: {
+//     website: 'AlgoExpert',
+//   },
+// };
+// log(person?.company?.website ?? 'Foo');
+
+// const shouldRunCode = true;
+// function logWorld() {
+//   log('Hello World');
+// }
+// if (shouldRunCode) {
+//   logWorld();
+// }
+// shouldRunCode && logWorld();
