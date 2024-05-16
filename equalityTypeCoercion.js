ff29c5ea78df8a398662556949ea9788dee95c2d
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2024-05-15 20:53:42 -0400

    Add Equality and type coercion

---
+++ equalityTypeCoercion.js
@@ -0,0 +1,27 @@
+import { log } from './log.js';
+
+// log(5 == 5);
+// log(5 == '5');
+// log(5 === 5);
+// log(5 === '5');
+
+// log(Number(false));
+// log(true == 1);
+// log(Boolean(10));
+// log(String(0));
+
+// log(Number('abc'));
+// log(parseInt('1abc'));
+// log(NaN == NaN);
+
+// const x = 5;
+// if (x == null) {
+//   // do something
+// }
+
+// log(null == undefined);
+// log(null == null);
+
+// const obj = {};
+// log(obj === obj);
+// log({} == {});
