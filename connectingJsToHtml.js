0aa6da44dc96a59bfbfbf51ff561e2271c5db13c
Andrey Lyamkin <andreylyamkin@Andreys-MacBook-Air.local>
2024-05-16 09:03:08 -0400

    Add Connecting javascript to html

---
+++ connectingJsToHtml.js
@@ -0,0 +1,17 @@
+import { log } from './log.js';
+
+// window.addEventListener('DOMContentLoaded', main);
+// window.addEventListener('load', main);
+
+// function main() {
+//   log('DOMContentLoaded');
+//   const button = document.querySelector('button');
+//   button.addEventListener('click', () => {
+//     document.body.style.backgroundColor = '#00334c';
+//   });
+// }
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  document.body.style.backgroundColor = '#00334c';
+});
